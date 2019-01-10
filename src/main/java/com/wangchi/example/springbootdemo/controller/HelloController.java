ab1a9cb232b212e9f8b13edbfdda2b1e079f26bd
wangchi <f2114198@163.com>
2019-01-10 09:56:38 +0800

    Initial commit

---
+++ src/main/java/com/wangchi/example/springbootdemo/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.wangchi.example.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello i'm spring boot";
+    }
+}
